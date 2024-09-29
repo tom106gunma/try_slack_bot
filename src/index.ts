51d3ba7f776b42f92a1f2c8589528a87592187eb
tom106gunma <tom106.net@gmail.com>
2024-09-30 07:05:46 +0900

    簡易的なbotを追加

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-console.log('Hello World!!');
+import { App } from '@slack/bolt';
+import 'dotenv/config'
+
+//②SlackBotのトークンとシークレットを環境変数から読み込む
+const env = process.env;
+
+//③SlackBotの初期化
+const app = new App({
+  token: env.SLACK_BOT_TOKEN,
+  signingSecret: env.SLACK_SIGNING_SECRET,
+});
+
+//④SlackBotにメッセージを送信する
+app.message('', async ({ message, say }) => {
+  if (!message.subtype) {
+    await say(`Hello, <@${message.user}>. You said: ${message.text}`);
+  }
+});
+
+(async () => {
+  //⑤SlackBotを起動する
+  await app.start(process.env.PORT || 3000);
+
+  console.log('⚡️ Bolt app is running!');
+})();
